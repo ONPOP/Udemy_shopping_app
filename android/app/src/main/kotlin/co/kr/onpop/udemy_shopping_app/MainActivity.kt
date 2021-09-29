e4f43b7b6bdfe4325bf6e812eccd87561bcc788f
ONPOP <onpoplive@onpop.co.kr>
2021-09-30 08:35:37 +0900

    first

---
+++ android/app/src/main/kotlin/co/kr/onpop/udemy_shopping_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.kr.onpop.udemy_shopping_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      theme: ThemeData(
+        primaryColor: Colors.amber,
+        textTheme: TextTheme(),
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
